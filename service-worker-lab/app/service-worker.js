a02f44cf4d03fd32a029a618caf4dfd4035caefb
Gabriel Bourget <gaboxa@syntronic.com>
2021-03-27 17:48:55 -0400

    service workers lab

--- service-worker-lab/app/service-worker.js
+++ service-worker-lab/app/service-worker.js
@@ -1,7 +1,12 @@
 self.addEventListener("install", (evt) => {
   console.log("[service-worker.js]: Service worker installed");
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (evt) => {
   console.log("[service-worker.js]: Service worker activated");
+});
+
+self.addEventListener('fetch', event => {
+  console.log('[service-worker.js]: Fetching -> ', event.request.url);
 });
