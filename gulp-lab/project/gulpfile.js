6a81a7ee1b80ef515073c51a5c2812ad30594b2e
Gabriel Bourget <gaboxa@syntronic.com>
2021-04-01 22:47:02 -0400

    gulp tutorial

--- gulp-lab/project/gulpfile.js
+++ gulp-lab/project/gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync");
 const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
 
 const copyFiles = () => {
   return (
     gulp.src([
       "app/*.html",
       "app/**/*.jpg",
-      "app/**/*.css",
     ])
     .pipe(gulp.dest("build"))
   );
@@ -18,10 +20,25 @@ gulp.task("copy", copyFiles);
 const processJS = () => {
   return gulp.src("app/scripts/*.js")
     .pipe(babel({ presets: ["env"] }))
-    .pipe(gulp.dest("build/scripts"));
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("build/scripts/"));
 }
 gulp.task("processJS", processJS);
 
+const watch = () => {
+  gulp.watch("app/scripts/*.js", gulp.series(processJS)),
+  gulp.watch("app/scripts/*.css", gulp.series(processCSS))
+}
+gulp.task("watch", watch);
+
+const processCSS = () => {
+  return gulp.src("app/styles/*.css")
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("build/styles/"));
+}
+gulp.task("processCSS", processCSS);
 
 const serveFiles = () => {
   return browserSync.init({
@@ -30,4 +47,10 @@ const serveFiles = () => {
     port: 3000
   });
 }
-gulp.task("buildAndServe", gulp.series(copyFiles, processJS, serveFiles));
+gulp.task(
+  "buildAndServe",
+  gulp.series(
+    copyFiles, processCSS, processJS,
+    gulp.parallel(serveFiles, watch)
+  )
+);
