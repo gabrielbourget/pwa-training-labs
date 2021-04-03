ae86f1ed7f8890b3bfbf84a1d64558f5a759b454
Gabriel Bourget <gaboxa@syntronic.com>
2021-04-03 01:20:48 -0400

    AMP lab

--- amp-pwa-lab/project/js/app.js
+++ amp-pwa-lab/project/js/app.js
@@ -17,7 +17,15 @@
 'use strict';
 class Router {
   replaceLinks(document) {
-    // TODO replace links
+    if ('serviceWorker' in navigator) {
+      return;
+    }
+    const elements = document.getElementsByTagName('a');
+    for (let i = 0; i < elements.length; i++) {
+      const anchor = elements[i];
+      const href = anchor.href;
+      anchor.href = '/shell.html#href=' + encodeURIComponent(href);
+    }
   }
 }
 
@@ -58,7 +66,12 @@ class AmpPage {
   };
 
   loadDocument(url) {
-    // TODO: Add code to load a document and attach to Shadow Root
+    return this._fetchDocument(url).then(document => {
+      router.replaceLinks(document);
+      const header = document.querySelector('.header');
+      header.remove();
+      window.AMP.attachShadowDoc(this.rootElement, document, url);
+    });
   }
 }
 
@@ -66,6 +79,24 @@ const ampReadyPromise = new Promise(resolve => {
   (window.AMP = window.AMP || []).push(resolve);
 });
 const router = new Router();
+router.replaceLinks(document);
 
-// TODO: get a reference to the container and URL, and load the AMP page
-// when ampReadyPromise resolves.
+function getContentUri() {
+  const hash = window.location.hash;
+  if (hash && hash.indexOf('href=') > -1) {
+    return decodeURIComponent(hash.substr(6));
+  }
+  return window.location;
+}
+
+const ampRoot = document.querySelector('#amproot');
+// const url = document.location.href;
+const url = getContentUri();
+const amppage = new AmpPage(ampRoot, router);
+ampReadyPromise.then(() => {
+  return amppage.loadDocument(url);
+}).then(() => {
+  if (window.history) {
+    window.history.replaceState({}, '', url);
+  }
+});
